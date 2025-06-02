711fb1d2a36338e7e813e18739bef626c7b766f6
Yuri Silva Herdt <yherdt@gmail.com>
2025-06-01 23:30:22 -0300

    Subindo exercícios do curso

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -1,6 +1,7 @@
 /*
 ////////////////////////////////////
 // Linking a JavaScript File
+
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,39 @@
+/*
+console.log(10 + 5);
+console.log("Jonas");
+
+let firstName = "teste";
+let first = "jonas";
+let firstNamePerson = "Jonas";
+console.log(firstName);
+//variables must not sstart with numbers like:
+// let 3yaers = 3; (wrong)
+// let jonas&yaers = 3; (also wrong because of especial caracters)
+//and there are some reserved keywords like funcion or name
+// variables must be descreptives.
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+console.log(myFirstJob);
+
+//not like that:
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+//Assingments:
+*/
+//Assingment 1
+let country = "Brazil";
+let continent = "South America";
+let population = 211;
+
+console.log(
+  "My country is " +
+    country +
+    ", witch is in " +
+    continent +
+    " and has a population of " +
+    population +
+    " millions of people."
+);
