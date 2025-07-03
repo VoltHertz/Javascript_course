271dd3fa96d6f16f266b1f68f26dd25cf7c75d5f
Yuri Silva Herdt <yherdt@gmail.com>
2025-07-02 21:54:10 -0300

    Adicionado challenge03

--- Coding Challanges/Challenge02.js
+++ Coding Challanges/Challenge02.js
@@ -23,4 +23,8 @@ const BMIMark = massMark / (heightMark * heightMark);
 const BMIJohn = massJohn / (heightJohn * heightJohn);
 console.log(BMIMark, BMIJohn);
 
-é
+if ( BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+}
